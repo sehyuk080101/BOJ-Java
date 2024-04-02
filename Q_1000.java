e9a4d432ad2346f738c7744ad051d84027f84924
sehyuk080101 <164876765+sehyuk080101@users.noreply.github.com>
2024-04-02 17:00:27 +0900

    자바 1000

---
+++ Q_1000.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Q_1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+b);
+    }
+}
